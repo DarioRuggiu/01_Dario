c19736af4c4f51b6633cbd1518116fbcd62c94a6
Dario Ruggiu <dario.rgg@hotmail.it>
2022-06-01 17:00:22 +0200

    Le modifiche su main.py sono dovute a diversi tentativi e dallo studio di fastAPI

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
+print(fake_items_db)
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 2):
+    return fake_items_db[skip : skip + limit]
