3a0265bc6bf81164b501ee6acc5fc3bd871dbeb4
Dario Ruggiu <dario.rgg@hotmail.it>
2022-06-01 17:11:13 +0200

    Ho continuato a fare alcune prove su main.py e poi ho creato conoscenza_json per imparare a conoscere il formato json

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 
 print(fake_items_db)
 @app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 2):
+async def read_item(skip: int = 0, limit: int = 1):
     return fake_items_db[skip : skip + limit]
+
