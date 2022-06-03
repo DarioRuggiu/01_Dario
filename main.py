a4f3e8149a28a911fd65b6f460bd8ae94dab5d87
Dario Ruggiu <dario.rgg@hotmail.it>
2022-06-03 13:38:04 +0200

    E' stato eliminato conoscenza_json che è stato sostituito da main

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+app.include_router(pioggia.router)
+app.include_router(temperatura.router)
+app.include_router(umidita.router)
 
-print(fake_items_db)
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 1):
-    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/{tipo}/{iniziofine}")
+async def root(tipo:str,iniziofine:str):
+    if tipo=="PR":
+        tipo_stringa="PRECP_MIN"
+    else:
+        tipo_stringa="TEMP_30MIN"
+    
+    return {"tipo_dato": tipo_stringa}
 
