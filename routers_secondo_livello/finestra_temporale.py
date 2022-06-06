39a22179abcbb7e97c31c1172f9a0ba5c6a861c0
Dario Ruggiu <dario.rgg@hotmail.it>
2022-06-06 10:49:39 +0200

    E' stato modificato il file finestr_temporale.py perchè c'erano degli errori nell'uso di datetime.strptime

--- routers_secondo_livello/finestra_temporale.py
+++ routers_secondo_livello/finestra_temporale.py
@@ -8,9 +8,9 @@ router_2 = APIRouter(
 
 @router_2.get('/')
 def print_text(finestra_temporale:str):
-    data_ini = datetime.strptime(finestra_temporale[:11],'%Y%M%d%h%M')
-    data_fin = datetime.strptime(finestra_temporale[12:],'%Y%M%d%h%M')
-    return "Verranno selezionati i dati a partire da {data_ini} fino a {data_fin}. Scegliere la tipologia di dato meteo associato alla stazione."
+    data_ini = datetime.strptime(finestra_temporale[:12],'%Y%m%d%H%M')
+    data_fin = datetime.strptime(finestra_temporale[12:],'%Y%m%d%H%M')
+    return "Verranno selezionati i dati a partire da %s fino a %s. Scegliere la tipologia di dato meteo associato alla stazione."%(data_ini,data_fin)
 
 router_2.include_router(pioggia.router_3)
 router_2.include_router(temperatura.router_3)
