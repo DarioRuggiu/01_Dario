5fed2ef97e7a725cfacb9f6dca8281a90afdc59e
Dario Ruggiu <dario.rgg@hotmail.it>
2022-06-07 10:07:48 +0200

    Modifiche per ritornare allo stato precedente <funzionante>

--- routers_secondo_livello/finestra_temporale.py
+++ routers_secondo_livello/finestra_temporale.py
@@ -11,7 +11,7 @@ router_2 = APIRouter(
 def print_text(finestra_temporale:str):
     data_ini = datetime.strptime(finestra_temporale[:12],'%Y%m%d%H%M')
     data_fin = datetime.strptime(finestra_temporale[12:],'%Y%m%d%H%M')
-    return "Verranno selezionati i dati a partire dal %s fino al %s. Scegliere la tipologia di dato meteo associato alla stazione.%i"%(data_ini,data_fin,x)
+    return "Verranno selezionati i dati a partire dal %s fino al %s. Scegliere la tipologia di dato meteo associato alla stazione."%(data_ini,data_fin)
 
 router_2.include_router(pioggia.router_3)
 router_2.include_router(temperatura.router_3)
